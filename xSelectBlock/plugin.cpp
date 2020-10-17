978cdbb97fbda1f11f607da7306d83642d8b7fd5
morsisko <morsisko@gmail.com>
2020-10-17 23:58:01 +0200

    template cleanup

--- xSelectBlock/plugin.cpp
+++ xSelectBlock/plugin.cpp
@@ -2,10 +2,8 @@
 
 enum
 {
-    MENU_TEST,
-    MENU_DISASM_ADLER32,
-    MENU_DUMP_ADLER32,
-    MENU_STACK_ADLER32
+    MENU,
+    DUMP,
 };
 
 static void Adler32Menu(int hWindow)
@@ -51,47 +49,16 @@ static duint exprZero(int argc, duint* argv, void* userdata)
     return 0;
 }
 
-PLUG_EXPORT void CBINITDEBUG(CBTYPE cbType, PLUG_CB_INITDEBUG* info)
-{
-    dprintf("Debugging of %s started!\n", info->szFileName);
-}
-
-PLUG_EXPORT void CBSTOPDEBUG(CBTYPE cbType, PLUG_CB_STOPDEBUG* info)
-{
-    dputs("Debugging stopped!");
-}
-
-PLUG_EXPORT void CBEXCEPTION(CBTYPE cbType, PLUG_CB_EXCEPTION* info)
-{
-    dprintf("ExceptionRecord.ExceptionCode: %08X\n", info->Exception->ExceptionRecord.ExceptionCode);
-}
-
-PLUG_EXPORT void CBDEBUGEVENT(CBTYPE cbType, PLUG_CB_DEBUGEVENT* info)
-{
-    if(info->DebugEvent->dwDebugEventCode == EXCEPTION_DEBUG_EVENT)
-    {
-        dprintf("DebugEvent->EXCEPTION_DEBUG_EVENT->%.8X\n", info->DebugEvent->u.Exception.ExceptionRecord.ExceptionCode);
-    }
-}
-
 PLUG_EXPORT void CBMENUENTRY(CBTYPE cbType, PLUG_CB_MENUENTRY* info)
 {
     switch(info->hEntry)
     {
-    case MENU_TEST:
+    case MENU:
         MessageBoxA(hwndDlg, "Test Menu Entry Clicked!", PLUGIN_NAME, MB_ICONINFORMATION);
         break;
 
-    case MENU_DISASM_ADLER32:
-        Adler32Menu(GUI_DISASSEMBLY);
-        break;
-
-    case MENU_DUMP_ADLER32:
+    case DUMP:
         Adler32Menu(GUI_DUMP);
-        break;
-
-    case MENU_STACK_ADLER32:
-        Adler32Menu(GUI_STACK);
         break;
 
     default:
@@ -115,8 +82,6 @@ void pluginStop()
 //Do GUI/Menu related things here.
 void pluginSetup()
 {
-    _plugin_menuaddentry(hMenu, MENU_TEST, "&Menu Test");
-    _plugin_menuaddentry(hMenuDisasm, MENU_DISASM_ADLER32, "&Adler32 Selection");
-    _plugin_menuaddentry(hMenuDump, MENU_DUMP_ADLER32, "&Adler32 Selection");
-    _plugin_menuaddentry(hMenuStack, MENU_STACK_ADLER32, "&Adler32 Selection");
+    _plugin_menuaddentry(hMenu, MENU, "xSelectBlock");
+    _plugin_menuaddentry(hMenuDump, DUMP, "&Select block");
 }
